6ea8d8b72928d09c9dcea6502e56423e2fe5bc03
IgorSiqueiraMBM <igor.siqueira@mbmsolutions.com.br>
2023-11-29 10:33:21 -0300

    ..

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -64,6 +64,7 @@ class _HomeState extends State<Home> {
                             setState(() {
                               db.toDoList[index][1] = !db.toDoList[index][1]!;
                             }),
+                            db.updateDataBase(),
                           },
                           value: db.toDoList[index][1],
                         ),
